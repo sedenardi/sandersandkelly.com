cba79d7f69fa26ef8d40bea0c0b6fa416ea2a80c
Sanders DeNardi <sedenardi@gmail.com>
2017-03-21 23:51:08 -0400

    add google map

---
+++ src/js/map.js
@@ -0,0 +1,45 @@
+/* global google */
+
+const places = [{
+  lat: 39.0400848,
+  lon: -77.6185989,
+  info: '<strong>Oatlands Historic House and Gardens</strong><br>20850 Oatlands Plantation Lane<br> Leesburg, VA 20175<br>'
+}, {
+  lat: 39.0798574,
+  lon: -77.47702,
+  info: '<strong>Lansdowne Resort and Spa</strong><br>44050 Woodridge Pkwy<br> Leesburg, VA 20176<br>'
+}, {
+  lat: 39.0931746,
+  lon: -77.5242336,
+  info: '<strong>Clarion Inn Historic Leesburg</strong><br>1500 East Market Street<br> Leesburg, VA 20176<br>'
+}];
+
+export default function map() {
+  window.initMap = function() {
+    map = new google.maps.Map(document.getElementById('map_canvas'), {
+  		center: new google.maps.LatLng(39.076341, -77.542037),
+  		zoom: 10,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+  	});
+
+    const infowindow = new google.maps.InfoWindow({});
+
+    places.forEach((p, i) => {
+      const marker = new google.maps.Marker({
+  			position: new google.maps.LatLng(p.lat, p.lon),
+  			map: map
+  		});
+      google.maps.event.addListener(marker, 'click', ((marker) => {
+  			return function () {
+  				infowindow.setContent(p.info);
+  				infowindow.open(map, marker);
+  			};
+  		})(marker));
+
+      if (i === 0) {
+        infowindow.setContent(p.info);
+        infowindow.open(map, marker);
+      }
+    });
+  };
+};
