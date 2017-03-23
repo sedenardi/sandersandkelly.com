5727f25b1f1e9578dc0d489e0605e1228886fb2f
Sanders DeNardi <sedenardi@gmail.com>
2017-03-22 22:31:20 -0400

    add directions link to map

--- src/js/map.js
+++ src/js/map.js
@@ -3,15 +3,15 @@
 const places = [{
   lat: 39.0400848,
   lon: -77.6185989,
-  info: '<strong>Oatlands Historic House and Gardens</strong><br>20850 Oatlands Plantation Lane<br> Leesburg, VA 20175<br>'
+  info: '<strong>Oatlands Historic House and Gardens</strong><br>20850 Oatlands Plantation Lane<br> Leesburg, VA 20175<br><a href="https://www.google.com/maps/dir//20850+Oatlands+Plantation+Ln,+Leesburg,+VA+20175" target="_blank">Get Directions</a>'
 }, {
   lat: 39.0798574,
   lon: -77.47702,
-  info: '<strong>Lansdowne Resort and Spa</strong><br>44050 Woodridge Pkwy<br> Leesburg, VA 20176<br>'
+  info: '<strong>Lansdowne Resort and Spa</strong><br>44050 Woodridge Pkwy<br> Leesburg, VA 20176<br><a href="https://www.google.com/maps/dir//44050+Woodridge+Pkwy,+Leesburg,+VA+20176" target="_blank">Get Directions</a>'
 }, {
   lat: 39.0931746,
   lon: -77.5242336,
-  info: '<strong>Clarion Inn Historic Leesburg</strong><br>1500 East Market Street<br> Leesburg, VA 20176<br>'
+  info: '<strong>Clarion Inn Historic Leesburg</strong><br>1500 East Market Street<br> Leesburg, VA 20176<br><a href="https://www.google.com/maps/dir//1500+E+Market+St,+Leesburg,+VA+20176" target="_blank">Get Directions</a>'
 }];
 
 export default function map() {
