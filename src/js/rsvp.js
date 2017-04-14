c2ad752721e08fd25e75e3c4a52c1d015f35e4a7
Sanders DeNardi <sedenardi@gmail.com>
2017-04-14 01:01:51 -0400

    hook up RSVP form

--- src/js/rsvp.js
+++ src/js/rsvp.js
@@ -1,6 +1,30 @@
 (function() {
-  document.getElementById('rsvp-form').addEventListener('submit', (e) => {
-    //e.preventDefault();
-    console.log(e);
+  const FormCarousel = require('form-carousel');
+
+  const postUrl = 'https://script.google.com/macros/s/AKfycbxJa91d83Bok5ChngfUPftsxG-X4EkZvpSSggwvpxfnNT7Zy7kw/exec';
+  const declineText = 'Thank you for letting us know. We\'re sorry to hear you can\'t make it.';
+
+  const submitForm = function(form) {
+    fetch(postUrl, {
+      method: 'POST',
+      body: new FormData(form)
+    });
+  };
+
+  const carousel = new FormCarousel('#rsvp-form', {
+    onSubmit: function(form) {
+      submitForm(form);
+      carousel.setSubmitted();
+    },
+    onEscape: function(input, form) {
+      if (input.value.trim().toLowerCase() === 'no') {
+        submitForm(form);
+        form.querySelector('.final-message').innerHTML = declineText;
+        carousel.setSubmitted();
+        return false;
+      } else {
+        return true;
+      }
+    }
   });
 })();
